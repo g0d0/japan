c53938474d35c3e2538454003c7b57dfada141e6
Eduardo Nakatsuka <enakatsuka@awesomemotive.com>
2022-11-27 19:57:01 -0300

    Remove console and also loadAttractions now will wait for files to be loaded, so it's not lost in the file

--- src/attractions.js
+++ src/attractions.js
@@ -2,12 +2,18 @@ import { fetchJsonAttractions, fetchTsvAttractions } from "./api";
 import { merge } from "./merge";
 import sortStrategy from "./sortStrategy";
 
-let arr1 = await fetchTsvAttractions();
-let arr2 = await fetchJsonAttractions();
-const data = merge(arr1, arr2);
+let data = [];
+
+const loadFiles = async () => {
+  const [tsvData, jsonData] = await Promise.all([
+    fetchTsvAttractions(),
+    fetchJsonAttractions()
+  ]);
+
+  data = merge(tsvData, jsonData);
+};
 
 const queryHandler = (data, query) => {
-  console.debug("Searching: ", query);
   if (query === "") return data;
     const filtered = data.filter((row) =>
       Object.values(row)
@@ -20,25 +26,20 @@ const queryHandler = (data, query) => {
 };
 
 const sortHandler = (data, column, order, offset) => {
-  console.debug("Ordering: ", { column, order, offset });
   const dataToDisplay = sortStrategy[column](data, column, order);
   return offsetHandler(dataToDisplay, 0, offset + 10);
 };
 
 const offsetHandler = (data, offset, limit) => {
-  console.debug("Cutting list: ", { offset, limit });
   return data.slice(offset, limit);
 };
 
 export const queryAttractions = (query, offset = 0) => {
-  console.debug("Searching attractions: ", { query, offset });
-  // we need to offset it as well
   let result = queryHandler(data, query);
   return offsetHandler(result, 0, offset + 10);
 };
 
 export const sortAttractions = (column, order, offset) => {
-  console.debug("Sorting attractions...");
   let result = data;
   const query = localStorage.getItem("query");
 
@@ -52,7 +53,6 @@ export const sortAttractions = (column, order, offset) => {
 };
 
 export const loadMoreAttractions = (offset) => {
-  console.debug("Loading more attractions...");
   if (localStorage.getItem("query")) {
     return queryAttractions(localStorage.getItem("query"));
   }
@@ -60,4 +60,7 @@ export const loadMoreAttractions = (offset) => {
   return offsetHandler(data, offset, offset + 10);
 };
 
-export const loadAttractions = () => offsetHandler(data, 0, 10);
+export const loadAttractions = async () => {
+  await loadFiles();
+  return offsetHandler(data, 0, 10)
+};
